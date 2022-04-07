b4d1f6528beec68abcc4330f8de90a79547bcab7
ftaylanne <taay.camyli@gmail.com>
2022-04-07 01:53:42 +0100

    first commit

---
+++ UsuariosWeb.Presentation/Models/UsuariosEdicaoModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace UsuariosWeb.Presentation.Models
+{
+    public class UsuariosEdicaoModel
+    {
+        public Guid IdUsuario { get; set; }
+
+        [Required(ErrorMessage = "Por favor, informe o nome do usuario.")]
+        public string Nome { get; set; }
+        
+        [Required(ErrorMessage = "Por favor, informe o email do usuario.")]
+        public string Email { get; set; }
+        
+        [Required(ErrorMessage = "Por favor, selecione o perfil do usuario.")]
+        public string IdPerfil { get; set; }
+
+        #region Campo de selecao do tipo DropDown/ComboBox
+
+        public List<SelectListItem>? ListagemPerfis { get; set; }
+        #endregion
+    }
+}
